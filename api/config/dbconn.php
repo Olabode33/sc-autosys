796975d4ff765e5ac9141c11cbd667a644241f34
Olabode A. Bello <olabode33@gmail.com>
2017-11-14 11:06:51 +0100

    Update dbconn.php

--- api/config/dbconn.php
+++ api/config/dbconn.php
@@ -3,10 +3,10 @@
 		function __construct(){}
 
 		//function db_connect($user='root', $password='P@ssw0rd', $db='sc_autosys_2')
-		function db_connect($user='sc_db_user', $password="HQ2dHZscf7KrncpZ", $db="sc_autosys_2")
+		function db_connect($user='sc_db_user', $password="#JS8ktQoTlDZKfTlj#", $db="sc_autosys_2")
 		{
 			//root P@ssw0rd
-			$conn = mysqli_connect('/cloudsql/involuted-bird-185511:us-central1:scautosys', $user, $password);
+			$conn = mysqli_connect('127.0.0.1', $user, $password);
 			if(!$conn) {
 				return 'Error connecting to database'; 
 			}
