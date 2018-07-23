e16cc3780c46eb81125a97aca4570c2c12417df7
Olabode A. Bello <olabode33@gmail.com>
2018-07-23 12:42:37 +0100

    Update database.php

--- habtob_FA/application/config/database.php
+++ habtob_FA/application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'sc_db_user',
+	'password' => '#JS8ktQoTlDZKfTlj#',
 	'database' => 'habtob_fc_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
