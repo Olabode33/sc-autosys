c1f0a4d4b7f2c966aae4458a3c27d793bab265f3
Olabode A. Bello <olabode33@gmail.com>
2018-07-25 19:29:09 +0100

    Add files via upload

--- habtob_FA/application/views/pages/home.php
+++ habtob_FA/application/views/pages/home.php
@@ -1,148 +1,148 @@
 <!--start-website-->
-	<!--Responsive-tabs-Starts-Here-->
-	</div>
-	<div class="website">
-		<div class="container">
-			<div class="website-top">
-				<h3>ABOUT OUR WEBSITE</h3>
-			</div>
-			<div class="sap_tabs">	
-				     <div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
-						  <ul class="resp-tabs-list">
-						  	  <li class="resp-tab-item" aria-controls="tab_item-0" role="tab"><span>What we do</span></li>
-							  <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><span>History</span></li>
-							  <li class="resp-tab-item" aria-controls="tab_item-2" role="tab"><span>News</span></li>
-							  <li class="resp-tab-item" aria-controls="tab_item-3" role="tab"><span>Clients</span></li>
-							  <div class="clear"></div>
-						  </ul>				  	 
-							<div class="resp-tabs-container">
-							    <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
-									<div class="facts">
-									  <img src="images/abt-img.png" alt="" />
-									  <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
-							        </div>
-							     </div>	
-							      <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
-									<div class="facts">
-									  <img src="images/abt-4.png" alt="" />
-									  <p>Printer took a galley of when an unknown type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
-							        </div>
-							     </div>	
-							      <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-2">
-									<div class="facts">
-									  <img src="images/abt-2.png" alt="" />
-									  <p>Took a galley of type and scrambled it to make awhen an unknown printer  type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
-							        </div>  
-							     </div>	
-							     <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-3">
-									<div class="facts">
-									  <img src="images/abt-3.png" alt="" />
-									  <p>Galley of type and scrambled when an unknown printer took a it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
-							        </div>     
-							     </div>	
-							 </div>
-					      </div>
-					 </div>
-					 </div>
-					</div> 
-					 <script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
-					    <script type="text/javascript">
-						    $(document).ready(function () {
-						        $('#horizontalTab').easyResponsiveTabs({
-						            type: 'default', //Types: default, vertical, accordion           
-						            width: 'auto', //auto or any width like 600px
-						            fit: true   // 100% fit in a container
-						        });
-						    });
-						   </script>
-					<!-- Partners Starts Here --->
-		<!--Responsive-tabs-ends-Here-->
-	<!--end-website-->
-	<!--start-special-->
-	<div class="special">
-		<div class="special-top">
-			<h3>SPECIAL CASES SOLVED</h3>
+    <!--Responsive-tabs-Starts-Here-->
+    </div>
+    <div class="website">
+        <div class="container">
+            <div class="website-top">
+                <h3>ABOUT HABTOB FA</h3>
+            </div>
+            <div class="sap_tabs">	
+                <div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+                    <ul class="resp-tabs-list">
+                        <li class="resp-tab-item" aria-controls="tab_item-0" role="tab"><span>What we do</span></li>
+                        <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><span>History</span></li>
+                        <li class="resp-tab-item" aria-controls="tab_item-2" role="tab"><span>News</span></li>
+                        <li class="resp-tab-item" aria-controls="tab_item-3" role="tab"><span>Clients</span></li>
+                        <div class="clear"></div>
+                    </ul>				  	 
+                    <div class="resp-tabs-container">
+                        <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
+                            <div class="facts">
+                                <img src="<?php echo base_url(); ?>assets/images/abt-img.png" alt="" />
+                                <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+                            </div>
+                        </div>	
+                        <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
+                            <div class="facts">
+                                <img src="<?php echo base_url(); ?>assets/images/abt-4.png" alt="" />
+                                <p>Printer took a galley of when an unknown type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+                            </div>
+                        </div>	
+                        <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-2">
+                            <div class="facts">
+                                <img src="<?php echo base_url(); ?>assets/images/abt-2.png" alt="" />
+                                <p>Took a galley of type and scrambled it to make awhen an unknown printer  type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+                            </div>  
+                        </div>	
+                        <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-3">
+                            <div class="facts">
+                                <img src="<?php echo base_url(); ?>assets/images/abt-3.png" alt="" />
+                                <p>Galley of type and scrambled when an unknown printer took a it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+                            </div>     
+                        </div>	
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+    <script src="<?php echo base_url(); ?>assets/js/easyResponsiveTabs.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#horizontalTab').easyResponsiveTabs({
+                type: 'default', //Types: default, vertical, accordion           
+                width: 'auto', //auto or any width like 600px
+                fit: true   // 100% fit in a container
+            });
+        });
+    </script>
+    <!-- Partners Starts Here --->
+    <!--Responsive-tabs-ends-Here-->
+<!--end-website-->
+    <!--start-special-->
+    <div class="special">
+        <div class="special-top">
+            <h3>OUR GALLERY</h3>
+        </div>
+            <div class="special-bottom">
+                <div class="col-md-6 special-bottom-left">
+                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
+                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-1.jpg" />
+                        <div class="b-wrapper">
+                            <h2 class="b-animate b-from-left b-from   b-delay03 ">
+                                <img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
+                                <span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+                            </h2>
+                        </div>
+                    </a>
 		</div>
-		<div class="special-bottom">
-			<div class="col-md-6 special-bottom-left">
-						<a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-									<img class="port-pic" class="img-responsive" src="images/one-1.jpg" />
-									<div class="b-wrapper">
-										<h2 class="b-animate b-from-left b-from   b-delay03 ">
-											<img src="images/spl-img.png" alt="">
-											<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
-										</h2>
-									</div>
-						</a>
+		<div class="col-md-6 special-bottom-left">
+                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
+                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-2.jpg" />
+			<div class="b-wrapper">
+                            <h2 class="b-animate b-from-left b-from   b-delay03 ">
+                            	<img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
+				<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+                            </h2>
 			</div>
-			<div class="col-md-6 special-bottom-left">
-						<a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-									<img class="port-pic" class="img-responsive" src="images/one-2.jpg" />
-									<div class="b-wrapper">
-										<h2 class="b-animate b-from-left b-from   b-delay03 ">
-											<img src="images/spl-img.png" alt="">
-											<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
-										</h2>
-									</div>
-						</a>
-			</div>
-			<div class="clearfix"> </div>
-			<div class="col-md-6 special-bottom-left">
-						<a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-									<img class="port-pic" class="img-responsive" src="images/one-3.jpg" />
-									<div class="b-wrapper">
-										<h2 class="b-animate b-from-left b-from   b-delay03 ">
-											<img src="images/spl-img.png" alt="">
-											<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
-										</h2>
-									</div>
-						</a>
-			</div>
-			<div class="col-md-6 special-bottom-left">
-						<a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-									<img class="port-pic" class="img-responsive" src="images/one-4.jpg" />
-									<div class="b-wrapper">
-										<h2 class="b-animate b-from-left b-from   b-delay03 ">
-											<img src="images/spl-img.png" alt="">
-											<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
-										</h2>
-									</div>
-						</a>
-			</div>
-			<div class="clearfix"> </div>
+                    </a>
 		</div>
+		<div class="clearfix"> </div>
+		<div class="col-md-6 special-bottom-left">
+                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
+			<img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-3.jpg" />
+			<div class="b-wrapper">
+                            <h2 class="b-animate b-from-left b-from   b-delay03 ">
+                            	<img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
+				<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+                            </h2>
+			</div>
+                    </a>
+		</div>
+		<div class="col-md-6 special-bottom-left">
+                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
+                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-4.jpg" />
+			<div class="b-wrapper">
+                            <h2 class="b-animate b-from-left b-from   b-delay03 ">
+                            	<img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
+				<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+                            </h2>
+			</div>
+                    </a>
+		</div>
+		<div class="clearfix"> </div>
+            </div>
 	</div>
 	<!--end-special-->
 	<!--start-news-->
 	<div class="news">
-		<div class="container">
-			<div class="news-top">
-				<h3>NEWS TO BELIEVE</h3>
-				<div class="news-bottom">
-					<div class="col-md-4 news-bottom-left">
-						<img src="images/news-1.jpg" alt="">
-						<p>Maecenas ut nunc auctor urna vestibulum cursus</p>
-						<div class="news-btn">
-							<a href="#">Readmore</a>
-						</div>
-					</div>
-					<div class="col-md-4 news-bottom-left">
-						<img src="images/news-2.jpg" alt="">
-						<p>Maecenas ut nunc auctor urna vestibulum cursus</p>
-						<div class="news-btn">
-							<a href="#">Readmore</a>
-						</div>
-					</div>
-					<div class="col-md-4 news-bottom-left">
-						<img src="images/news-3.jpg" alt="">
-						<p>Maecenas ut nunc auctor urna vestibulum cursus</p>
-						<div class="news-btn">
-							<a href="#">Readmore</a>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
+            <div class="container">
+            	<div class="news-top">
+                    <h3>LATEST ACADEMY NEWS</h3>
+                    <div class="news-bottom">
+                        <div class="col-md-4 news-bottom-left">
+                            <img src="<?php echo base_url(); ?>assets/images/news-1.jpg" alt="">
+                            <p>Maecenas ut nunc auctor urna vestibulum cursus</p>
+                            <div class="news-btn">
+				<a href="#">Readmore</a>
+                            </div>
 			</div>
+			<div class="col-md-4 news-bottom-left">
+                            <img src="<?php echo base_url(); ?>assets/images/news-2.jpg" alt="">
+                            <p>Maecenas ut nunc auctor urna vestibulum cursus</p>
+                            <div class="news-btn">
+                                <a href="#">Readmore</a>
+                            </div>
+			</div>
+			<div class="col-md-4 news-bottom-left">
+                            <img src="<?php echo base_url(); ?>assets/images/news-3.jpg" alt="">
+                            <p>Maecenas ut nunc auctor urna vestibulum cursus</p>
+                            <div class="news-btn">
+                                <a href="#">Readmore</a>
+                            </div>
+			</div>
+			<div class="clearfix"></div>
+                    </div>
 		</div>
+            </div>
 	</div>
 	<!--end-news-->
