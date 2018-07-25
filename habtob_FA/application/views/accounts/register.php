2180e1163ee9c620995404dfc7286034c607b9be
Olabode A. Bello <olabode33@gmail.com>
2018-07-25 19:30:19 +0100

    Add files via upload

--- habtob_FA/application/views/accounts/register.php
+++ habtob_FA/application/views/accounts/register.php
@@ -1,18 +1,22 @@
 </div>
-<div class="website">
+<div class="website" style="margin-bottom: 3em">
     <div class="container">
         <div class="website-top">
             <h3>REGISTER WITH US</h3>
         </div>
-        <div class="">
+        <div class="text-danger">
             <?php echo validation_errors(); ?>
         </div>
 
         <?php echo form_open('accounts/register') ?>
         <div class="form-horizontal">
+            <legend>Personal Details</legend>
             <div class="form-group">
                 <div class="col-md-4">
-                    <label class="control-label">First Name</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        First Name
+                    </label>
                     <input type="text" name="f_name" class="form-control" placeholder="First Name" required/>
                 </div>
                 <div class="col-md-4">
@@ -20,18 +24,27 @@
                     <input type="text" name="m_name" class="form-control" placeholder="Middle Name"/>
                 </div>
                 <div class="col-md-4">
-                    <label class="control-label">Last Name</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Last Name
+                    </label>
                     <input type="text" name="l_name" class="form-control" placeholder="Last Name" required/>
                 </div>
             </div>
             
             <div class="form-group">
                 <div class="col-md-4">
-                    <label class="control-label">Date of Birth</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Date of Birth
+                    </label>
                     <input type="date" name="dob" class="form-control" required/>
                 </div>
                 <div class="col-md-4">
-                    <label class="control-label">Gender</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Gender
+                    </label>
                     <select class="form-control" name="gender" required>
                         <option value="">-- Please Select --</option>
                         <option value="Male">Male</option>
@@ -51,7 +64,7 @@
             <div class="form-group">
                 <div class="col-md-4">
                     <label class="control-label">Address</label>
-                    <input type="text" name="address" class="form-control" placeholder="First Name" required/>
+                    <input type="text" name="address" class="form-control" placeholder="Address" />
                 </div>
                 <div class="col-md-4">
                     <label class="control-label">LGA</label>
@@ -73,17 +86,20 @@
             <div class="form-group">
                 <div class="col-md-4">
                     <label class="control-label">Contact Phone</label>
-                    <input type="text" name="contact_phone" class="form-control" placeholder="+234 xxx-xxx-xxxx" required=""/>
+                    <input type="text" name="contact_phone" class="form-control" placeholder="+234 xxx-xxx-xxxx"/>
                 </div>
                 <div class="col-md-4">
                     <label class="control-label">Contact Email</label>
-                    <input type="email" name="contact_email" class="form-control" placeholder="xxx@xxx.xxx" required/>
+                    <input type="email" name="contact_email" class="form-control" placeholder="xxx@xxx.xxx"/>
                 </div>
             </div>
             
             <div class="form-group">
                 <div class="col-md-4">
-                    <label class="control-label">Are you a student?</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Are you a student?
+                    </label>
                     <select class="form-control" name="is_student" required>
                         <option value="">-- Please Select --</option>
                         <option value="Y">Yes</option>
@@ -91,7 +107,10 @@
                     </select>
                 </div>
                 <div class="col-md-4">
-                    <label class="control-label">Highest level of Education</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Highest level of Education
+                    </label>
                     <select class="form-control" name="education_level" required>
                         <option value="">-- Please Select --</option>
                         <option value="Primary">Primary School</option>
@@ -109,11 +128,17 @@
             
             <div class="form-group">
                 <div class="col-md-8">
-                    <label class="control-label">Why do you want to join us?</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Why do you want to join us?
+                    </label>
                     <textarea name="reason_to_join" class="form-control" required></textarea>
                 </div>
                 <div class="col-md-4">
-                    <label class="control-label">Preferred Position</label>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Preferred Position
+                    </label>
                     <select class="form-control" name="preferred_position" required>
                         <option value="">-- Please Select --</option>
                         <option value="Forward">Forward</option>
@@ -126,45 +151,64 @@
                 </div>
             </div>
             
+            <hr>
+            <legend>Guardian's Details</legend>            
             <div class="form-group">
                 <div class="col-md-4">
-                    <label class="control-label">Guardian's Full Name</label>
-                    <input type="text" name="guardian_full_name" class="form-control" placeholder="Guardian's Full Name" required=""/>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Guardian's Full Name
+                    </label>
+                    <input type="text" name="guardian_full_name" id="guardian_full_name" class="form-control" placeholder="Guardian's Full Name" required=""/>
                 </div>
                 <div class="col-md-4">
-                    <label class="control-label">Guardian's Phone number</label>
-                    <input type="text" name="guardian_phone" class="form-control" placeholder="+234 xxx-xxx-xxxx" required/>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Guardian's Phone number
+                    </label>
+                    <input type="text" name="guardian_phone" id="guardian_phone" class="form-control" placeholder="+234 xxx-xxx-xxxx" required/>
                 </div>
                 <div class="col-md-4">
-                    <label class="control-label">Guardian's Email</label>
-                    <input type="email" name="guardian_email" class="form-control" placeholder="xxx@xxx.xxx" required=""/>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Guardian's Email
+                    </label>
+                    <input type="email" name="guardian_email" id="guardian_email" class="form-control" placeholder="xxx@xxx.xxx" required=""/>
                 </div>
             </div>
             <div class="form-group">
                 <div class="col-md-12">
-                    <label class="control-label">Guardian's Address</label>
-                    <input type="text" name="guardian_address" class="form-control" placeholder="Guardian's Address" required=""/>
+                    <label class="control-label">
+                        <span class="text-danger">*</span>
+                        Guardian's Address
+                    </label>
+                    <input type="text" name="guardian_address" id="guardian_address" class="form-control" placeholder="Guardian's Address" required=""/>
                 </div>
             </div>
             
+            <hr>
+            <legend>Next of Kin Details</legend>
             <div class="form-group">
+                <div class="col-md-12">
+                    <button class="btn btn-warning btn-xs" id="btn_copy_guardian_2_nok">Same as guardian</button>
+                </div>
                 <div class="col-md-4">
                     <label class="control-label">Next of Kin's Full Name</label>
-                    <input type="text" name="nok_name" class="form-control" placeholder="Next of Kin's Full Name"/>
+                    <input type="text" name="nok_name" id="nok_name" class="form-control" placeholder="Next of Kin's Full Name"/>
                 </div>
                 <div class="col-md-4">
                     <label class="control-label">Next of Kin's Phone number</label>
-                    <input type="text" name="nok_phone" class="form-control" placeholder="+234 xxx-xxx-xxxx"/>
+                    <input type="text" name="nok_phone" id="nok_phone" class="form-control" placeholder="+234 xxx-xxx-xxxx"/>
                 </div>
                 <div class="col-md-4">
                     <label class="control-label">Next of Kin's Email</label>
-                    <input type="email" name="nok_email" class="form-control" placeholder="xxx@xxx.xxx"/>
+                    <input type="email" name="nok_email" id="nok_email" class="form-control" placeholder="xxx@xxx.xxx"/>
                 </div>
             </div>
             <div class="form-group">
                 <div class="col-md-12">
                     <label class="control-label">Next of Kin's Address</label>
-                    <input type="text" name="nok_address" class="form-control" placeholder="Next of Kin's Address"/>
+                    <input type="text" name="nok_address" id="nok_address" class="form-control" placeholder="Next of Kin's Address"/>
                 </div>
             </div>
             
@@ -178,3 +222,12 @@
     </div>
 </div>
 
+<script>
+    $("#btn_copy_guardian_2_nok").click(function(event){
+        event.preventDefault();
+        $("#nok_name").val($("#guardian_full_name").val());		
+        $("#nok_phone").val($("#guardian_phone").val());		
+        $("#nok_email").val($("#guardian_email").val());		
+        $("#nok_address").val($("#guardian_address").val());
+    });
+</script>
