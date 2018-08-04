a0860b9f9a0506f6b98784687b2b59dbc58f3cb0
Olabode A. Bello <olabode33@gmail.com>
2018-08-04 11:34:01 +0100

    Add files via upload

--- habtob_FA/application/views/pages/home.php
+++ habtob_FA/application/views/pages/home.php
@@ -8,36 +8,58 @@
             </div>
             <div class="sap_tabs">	
                 <div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
-                    <ul class="resp-tabs-list">
-                        <li class="resp-tab-item" aria-controls="tab_item-0" role="tab"><span>What we do</span></li>
-                        <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><span>History</span></li>
-                        <li class="resp-tab-item" aria-controls="tab_item-2" role="tab"><span>News</span></li>
-                        <li class="resp-tab-item" aria-controls="tab_item-3" role="tab"><span>Clients</span></li>
+                    <ul class="resp-tabs-list row">
+                        <li class="resp-tab-item col-sm-3" aria-controls="tab_item-0" role="tab"><span>What we do</span></li>
+                        <li class="resp-tab-item col-sm-3" aria-controls="tab_item-1" role="tab"><span>Vision</span></li>
+                        <li class="resp-tab-item col-sm-3" aria-controls="tab_item-2" role="tab"><span>Mission</span></li>
+                        <li class="resp-tab-item col-sm-3" aria-controls="tab_item-3" role="tab"><span>Our Team</span></li>
                         <div class="clear"></div>
                     </ul>				  	 
                     <div class="resp-tabs-container">
                         <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
                             <div class="facts">
-                                <img src="<?php echo base_url(); ?>assets/images/abt-img.png" alt="" />
-                                <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+                                <p>   
+                                    <br>
+                                    Habtob Football Academy strives to create and nurture a 
+                                    valuable soccer learning experience where seasoned professional 
+                                    coaches develop skills, fitness, teamwork and sportsmanship 
+                                    to foster and accelerate soccer growth.
+                                </p>         
                             </div>
                         </div>	
                         <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
                             <div class="facts">
-                                <img src="<?php echo base_url(); ?>assets/images/abt-4.png" alt="" />
-                                <p>Printer took a galley of when an unknown type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+<!--                                <img src="<?php echo base_url(); ?>assets/images/abt-4.png" alt="" />-->
+                                <p>
+                                    <br>
+                                    To create and nurture a global and valuable football 
+                                    learning environment for the next generation. 
+                                </p>         
                             </div>
                         </div>	
                         <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-2">
                             <div class="facts">
-                                <img src="<?php echo base_url(); ?>assets/images/abt-2.png" alt="" />
-                                <p>Took a galley of type and scrambled it to make awhen an unknown printer  type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+<!--                                <img src="<?php echo base_url(); ?>assets/images/abt-2.png" alt="" />-->
+                                <p>
+                                    <br>
+                                    Habtob Football Academy exists to provide the highest level
+                                    of quality football training to our players, improve the 
+                                    moral standards of the kids in this era of moral decadence 
+                                    through regular classroom courses and to enhance their 
+                                    social/emotional and physical health, in a positive 
+                                    and safe environment
+                                </p>         
                             </div>  
                         </div>	
                         <div class="tab-1 resp-tab-content" aria-labelledby="tab_item-3">
                             <div class="facts">
-                                <img src="<?php echo base_url(); ?>assets/images/abt-3.png" alt="" />
-                                <p>Galley of type and scrambled when an unknown printer took a it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>         
+                                <p>
+                                    <br>
+                                    <b>Sponsor:</b><br>
+                                    Alhaji Habeeb Okunola<br>
+                                    <b>Coordinator:</b><br>
+                                    Ambrose Okere
+                                </p>
                             </div>     
                         </div>	
                     </div>
@@ -57,7 +79,7 @@
     </script>
     <!-- Partners Starts Here --->
     <!--Responsive-tabs-ends-Here-->
-<!--end-website-->
+<!--end-website 3865 x 1844 -->
     <!--start-special-->
     <div class="special">
         <div class="special-top">
@@ -65,46 +87,46 @@
         </div>
             <div class="special-bottom">
                 <div class="col-md-6 special-bottom-left">
-                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-1.jpg" />
+                    <a href="<?php echo site_url('news/one'); ?>" class="b-link-stripe b-animate-go  thickbox">
+                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/htfa_img_6_1.jpg" />
                         <div class="b-wrapper">
                             <h2 class="b-animate b-from-left b-from   b-delay03 ">
                                 <img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
-                                <span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+                                <span>Inauguration and Launching - 28th July 2018</span>
                             </h2>
                         </div>
                     </a>
 		</div>
 		<div class="col-md-6 special-bottom-left">
-                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-2.jpg" />
+                    <a href="<?php echo site_url('news/one'); ?>" class="b-link-stripe b-animate-go  thickbox">
+                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/htfa_img_5.jpg" />
 			<div class="b-wrapper">
                             <h2 class="b-animate b-from-left b-from   b-delay03 ">
                             	<img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
-				<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+				<span>Inauguration and Launching - 28th July 2018</span>
                             </h2>
 			</div>
                     </a>
 		</div>
 		<div class="clearfix"> </div>
 		<div class="col-md-6 special-bottom-left">
-                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-			<img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-3.jpg" />
+                    <a href="<?php echo site_url('news/one'); ?>" class="b-link-stripe b-animate-go  thickbox">
+			<img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/htfa_img_3.jpg" />
 			<div class="b-wrapper">
                             <h2 class="b-animate b-from-left b-from   b-delay03 ">
                             	<img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
-				<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+				<span>Inauguration and Launching - 28th July 2018</span>
                             </h2>
 			</div>
                     </a>
 		</div>
 		<div class="col-md-6 special-bottom-left">
-                    <a href="single.html" class="b-link-stripe b-animate-go  thickbox">
-                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/one-4.jpg" />
+                    <a href="<?php echo site_url('news/one'); ?>" class="b-link-stripe b-animate-go  thickbox">
+                        <img class="port-pic" class="img-responsive" src="<?php echo base_url(); ?>assets/images/htfa_img_4.jpg" />
 			<div class="b-wrapper">
                             <h2 class="b-animate b-from-left b-from   b-delay03 ">
                             	<img src="<?php echo base_url(); ?>assets/images/spl-img.png" alt="">
-				<span>normal distribution of letters, as opposed to using 'Content here, content here',</span>
+				<span>Inauguration and Launching - 28th July 2018</span>
                             </h2>
 			</div>
                     </a>
@@ -120,24 +142,16 @@
                     <h3>LATEST ACADEMY NEWS</h3>
                     <div class="news-bottom">
                         <div class="col-md-4 news-bottom-left">
-                            <img src="<?php echo base_url(); ?>assets/images/news-1.jpg" alt="">
-                            <p>Maecenas ut nunc auctor urna vestibulum cursus</p>
+                            <p>Inauguration and Launching - 28th July 2018</p>
                             <div class="news-btn">
-				<a href="#">Readmore</a>
+				<a href="<?php echo site_url('news/one'); ?>">Readmore</a>
                             </div>
 			</div>
 			<div class="col-md-4 news-bottom-left">
-                            <img src="<?php echo base_url(); ?>assets/images/news-2.jpg" alt="">
-                            <p>Maecenas ut nunc auctor urna vestibulum cursus</p>
+<!--                        <img src="<?php echo base_url(); ?>assets/images/news-2.jpg" alt="">-->
+                            <p>Player Registration and Trials</p>
                             <div class="news-btn">
-                                <a href="#">Readmore</a>
-                            </div>
-			</div>
-			<div class="col-md-4 news-bottom-left">
-                            <img src="<?php echo base_url(); ?>assets/images/news-3.jpg" alt="">
-                            <p>Maecenas ut nunc auctor urna vestibulum cursus</p>
-                            <div class="news-btn">
-                                <a href="#">Readmore</a>
+                                <a href="<?php echo site_url('news/two'); ?>">Readmore</a>
                             </div>
 			</div>
 			<div class="clearfix"></div>
